7e51ab723a4e105d8c65d26812344943ef3c9124
Adam Hutchison <liveh2o@gmail.com>
2011-03-24 23:53:35 -0500

    Now that we have FKs, we can't simply delete_all. We need to use destroy_all instead in order to get the benefit of nullifying the FKs. Added a couple of associations to people to make this work properly

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :comments, :foreign_key => :created_by, :dependent => :nullify
+  has_many :edited_comments, :class_name => 'Comment', :foreign_key => :updated_by, :dependent => :nullify
+  has_many :edited_ideas, :class_name => 'AbstractIdea', :foreign_key => :updated_by, :dependent => :nullify
   has_many :ideas, :foreign_key => :created_by, :dependent => :nullify
   has_many :votes, :dependent => :destroy
   
