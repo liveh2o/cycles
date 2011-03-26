dc2c18bf9994329a6ef2ea099b0f8ad5f6f1a056
Adam Hutchison <liveh2o@gmail.com>
2011-03-26 14:55:05 -0500

    Added associations to Person for various ideas (via identity columns)

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,12 @@
 class Person < ActiveRecord::Base
   has_many :comments, :foreign_key => :created_by, :dependent => :nullify
   has_many :edited_comments, :class_name => 'Comment', :foreign_key => :updated_by, :dependent => :nullify
+  has_many :abstract_ideas, :foreign_key => :created_by, :dependent => :nullify
+  has_many :cycling_ideas, :foreign_key => :created_by, :dependent => :nullify
   has_many :edited_ideas, :class_name => 'AbstractIdea', :foreign_key => :updated_by, :dependent => :nullify
   has_many :ideas, :foreign_key => :created_by, :dependent => :nullify
+  has_many :implemented_ideas, :foreign_key => :created_by, :dependent => :nullify
+  has_many :scrapped_ideas, :foreign_key => :created_by, :dependent => :nullify
   has_many :votes, :dependent => :destroy
   
   attr_accessible :name, :email
